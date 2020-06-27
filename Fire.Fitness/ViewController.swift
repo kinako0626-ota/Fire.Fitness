c309f2811f8af3f5f4fd4e42c3bd81eb10d92279
ryunosuke <kr07031321@gmail.com>
2020-06-27 23:45:55 +0900

    Initial Commit

---
+++ Fire.Fitness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fire.Fitness
+//
+//  Created by 太田　龍乃介 on 2020/06/27.
+//  Copyright © 2020 太田　龍乃介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
